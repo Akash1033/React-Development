3a9d31dddb0d59c60e82c6f2852a12bf7ff1c304
Akashdeep <akashvohra9877@gmail.com>
2026-02-03 10:23:04 +0530

    fixes

--- 07reactRouter/src/components/Home/Home.jsx
+++ 07reactRouter/src/components/Home/Home.jsx
@@ -18,7 +18,7 @@ export default function Home() {
                             <svg
                                 fill="white"
                                 width="24"
-                                height="24"
+                                height="24" 
                                 xmlns="http://www.w3.org/2000/svg"
                                 fillRule="evenodd"
                                 clipRule="evenodd"
